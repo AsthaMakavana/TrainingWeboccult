0b1cf04ddc471064673d803b3e7adb15793604a2
AsthaMakavana <97657964+AsthaMakavana@users.noreply.github.com>
2022-01-13 16:26:39 +0530

    update

--- Assignment_4_10-01-2022/task3/pattern3.js
+++ Assignment_4_10-01-2022/task3/pattern3.js
@@ -3,15 +3,9 @@
 var n = 5;
 var string = "";
 for (i = 1; i <= n; i++) {
-    for (j = 1; j <= n; j++) {
-        if(i-Math.abs(j)>=0) {
-            string += i-Math.abs(j)+1;
-        }
-        else {
-            string += " ";
-        }
-    
-    }
-    string += "\n"
+   for (j = 1; j <= n; j++) {
+       string += i;
+   }
+   string += "\n"
 }
 console.log(string);
